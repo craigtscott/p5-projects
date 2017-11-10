1341c50a46c646bd7df7f4a01427fed31a5315cf
Craig Scott <craigtscottjr@gmail.com>
2017-11-09 23:29:44 -0800

    srs: cleaned up rotating ship

--- spacerockshooter/spacerockshooter/ship.js
+++ spacerockshooter/spacerockshooter/ship.js
@@ -8,21 +8,22 @@ function Ship() {
 
   this.drawShip = function() {
     stroke(255);
-    // noFill();
-    // translate(300, 300);
+    this.rotate();
+
     line(this.center[0], this.center[1], this.end[0], this.end[1]);
-    // rotate(10);
-    // triangle(this.pos[0], this.pos[1], this.pos[2], this.pos[3], this.pos[4], this.pos[5]);
   };
 
-  this.rotate = function(deg) {
+  this.rotate = function() {
+    if (keyIsDown(LEFT_ARROW))
+    this.theta = -(PI/20);
+    if (keyIsDown(RIGHT_ARROW))
+    this.theta = (PI/20);
     x = this.end[0];
     y = this.end[1];
-    this.theta += deg;
 
-    this.end[0] = cos(deg) * (x-this.center[0]) - sin(deg) * (y-this.center[1]) + this.center[0];
+    this.end = [cos(this.theta) * (x-this.center[0]) - sin(this.theta) * (y-this.center[1]) + this.center[0], sin(this.theta) * (x-this.center[0]) + cos(this.theta) * (y-this.center[1]) + this.center[1]];
 
-    this.end[1] = sin(deg) * (x-this.center[0]) + cos(deg) * (y-this.center[1]) + this.center[1];
+    this.theta = 0;
 
 
   };
--- spacerockshooter/spacerockshooter/sketch.js
+++ spacerockshooter/spacerockshooter/sketch.js
@@ -13,10 +13,10 @@ function draw() {
   s.drawShip();
 }
 
-function keyPressed() {
-  if (keyCode ===LEFT_ARROW) {
-    s.rotate(-PI/20);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.rotate(PI/20);
-  }
-}
+// function key() {
+//   if (keyCode ===LEFT_ARROW) {
+//     s.rotate(-PI/20);
+//   } else if (keyCode === RIGHT_ARROW) {
+//     s.rotate(PI/20);
+//   }
+// }
