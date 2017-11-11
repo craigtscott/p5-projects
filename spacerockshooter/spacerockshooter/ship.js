5ac9394bebc5f32deb8495ff556218333e3c73c8
Craig Scott <craigtscottjr@gmail.com>
2017-11-10 21:09:03 -0800

    srs: working out movemnt

--- spacerockshooter/spacerockshooter/ship.js
+++ spacerockshooter/spacerockshooter/ship.js
@@ -5,15 +5,17 @@ function Ship() {
   this.center = [300,300];
   this.end = [300, 250];
   this.theta = 0;
+  this.direction = 0;
+  this.speed = 0;
 
   this.drawShip = function() {
     stroke(255);
-    this.rotate();
-
+    this.move();
+    this.fire();
     line(this.center[0], this.center[1], this.end[0], this.end[1]);
   };
 
-  this.rotate = function() {
+  this.move = function() {
     if (keyIsDown(LEFT_ARROW))
     this.theta = -(PI/20);
     if (keyIsDown(RIGHT_ARROW))
@@ -21,10 +23,23 @@ function Ship() {
     x = this.end[0];
     y = this.end[1];
 
+    // this.center =
+
     this.end = [cos(this.theta) * (x-this.center[0]) - sin(this.theta) * (y-this.center[1]) + this.center[0], sin(this.theta) * (x-this.center[0]) + cos(this.theta) * (y-this.center[1]) + this.center[1]];
 
+    this.direction += this.theta;
     this.theta = 0;
+  };
 
+  this.fire = function() {
+    if (keyCode === 32) {
 
+      stroke(255);
+      xmovment = cos(this.theta);
+      ymovment = sin(this.theta);
+      console.log(xmovment);
+      console.log(ymovment);
+      point(this.end[0]+ xmovment, this.end[1] + ymovment);
+    }
   };
 }
