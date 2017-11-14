1d05678ff097bf1558de9a1edb63f751f23e6939
Craig Scott <craigtscottjr@gmail.com>
2017-11-14 15:34:44 -0800

    srs: rock moves

--- spacerockshooter/spacerockshooter/rock.js
+++ spacerockshooter/spacerockshooter/rock.js
@@ -2,12 +2,19 @@ function Rock() {
 
   this.center = createVector(random(width), random(height));
   this.r = 75;
+  this.direction = random( PI * 2);
+  this.vel = createVector(1,0);
 
   this.render = function() {
+    this.move();
     translate(this.center.x, this.center.y);
     stroke(255);
     noFill();
     ellipse(0,0, this.r);
+  };
+
+  this.move = function() {
+    this.center.add(this.vel);
   };
 
 }
