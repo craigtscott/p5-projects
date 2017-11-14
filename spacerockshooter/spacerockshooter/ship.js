9673d3c40461add9903a258b00bfc7a1cff15f71
Craig Scott <craigtscottjr@gmail.com>
2017-11-13 22:14:24 -0800

    srs: refactors ship object

--- spacerockshooter/spacerockshooter/ship.js
+++ spacerockshooter/spacerockshooter/ship.js
@@ -2,44 +2,34 @@ function Ship() {
   // this.pos = [300, 240, 240, 360, 360, 360];
   // this.center = [(this.pos[2] + this.pos[4]) /2, (this.pos[1] + this.pos[3]) /2];
   // this.dir = PI/3.0;
-  this.center = [300,300];
-  this.end = [300, 250];
-  this.theta = 0;
+  this.center = createVector(300,300);
   this.direction = 0;
+  this.r = 20;
   this.speed = 0;
 
   this.drawShip = function() {
-    stroke(255);
+    translate(this.center.x, this.center.y);
     this.move();
-    this.fire();
-    line(this.center[0], this.center[1], this.end[0], this.end[1]);
+    rotate(this.direction);
+    stroke(255);
+    noFill();
+    // this.fire();
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
   };
 
   this.move = function() {
     if (keyIsDown(LEFT_ARROW))
-    this.theta = -(PI/20);
+
+    this.direction += -(PI/20);
     if (keyIsDown(RIGHT_ARROW))
-    this.theta = (PI/20);
-    x = this.end[0];
-    y = this.end[1];
+    this.direction += (PI/20);
 
     // this.center =
 
-    this.end = [cos(this.theta) * (x-this.center[0]) - sin(this.theta) * (y-this.center[1]) + this.center[0], sin(this.theta) * (x-this.center[0]) + cos(this.theta) * (y-this.center[1]) + this.center[1]];
+    // this.end = [cos(this.theta) * (x-this.center[0]) - sin(this.theta) * (y-this.center[1]) + this.center[0], sin(this.theta) * (x-this.center[0]) + cos(this.theta) * (y-this.center[1]) + this.center[1]];
 
-    this.direction += this.theta;
-    this.theta = 0;
+    // this.direction += this.theta;
+    // this.theta = 0;
   };
 
-  this.fire = function() {
-    if (keyCode === 32) {
-
-      stroke(255);
-      xmovment = cos(this.theta);
-      ymovment = sin(this.theta);
-      console.log(xmovment);
-      console.log(ymovment);
-      point(this.end[0]+ xmovment, this.end[1] + ymovment);
-    }
-  };
 }
