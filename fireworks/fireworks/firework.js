18714b92384c5ef3f7d95f4a7d32cb86f796bffe
Craig Scott <craigtscottjr@gmail.com>
2017-12-05 09:53:33 -0800

    fireword: fire works explode on circle or squair

--- fireworks/fireworks/firework.js
+++ fireworks/fireworks/firework.js
@@ -1,19 +1,41 @@
 function Firework(){
-  this.firework = new Particle(random(width), height);
+  this.firework = new Particle(random(width), height, false);
+  this.exploded = false;
+  this.shower = [];
 
   this.update = function() {
-    if (this.firework){
+    if (!this.exploded){
       this.firework.applyForce(gravity);
       this.firework.update();
       if (this.firework.vel.y >= 0) {
-        this.firework = null;
+        this.exploded = true;
+        this.explod();
       }
+    }
+    for (var i = 0; i < this.shower.length; i++) {
+      this.shower[i].applyForce(gravity);
+      this.shower[i].update();
     }
   };
 
-  this.draw= function() {
-    if (this.firework) {
+  this.explod = function() {
+    var shape = "square";
+
+    if (random(1) >= .5){
+      shape ="circle";
+    }
+    for (var i = 0; i < 100; i++) {
+      var part = new Particle(this.firework.pos.x, this.firework.pos.y, true, shape);
+      this.shower.push(part);
+    }
+  };
+
+  this.draw = function() {
+    if (!this.exploded) {
       this.firework.draw();
+    }
+    for (var i = 0; i < this.shower.length; i++) {
+      this.shower[i].draw();
     }
   };
 }
--- fireworks/fireworks/particle.js
+++ fireworks/fireworks/particle.js
@@ -1,19 +1,27 @@
-function Particle(x,y) {
+function Particle(x,y, shower, shape) {
   this.pos = createVector(x,y);
-  this.vel = createVector(0,random(-6,-20));
+  if (shower){
+    if (shape === "circle"){
+        this.vel = createVector(random(-.5,3), random(-.5,3));
+      } else {
+        this.vel = p5.Vector.random2D();
+      }
+  } else {
+    this.vel = createVector(random(-1, 1),random(-6,-20));
+  }
   this.acc = createVector(0,0);
-  
+
   this.applyForce = function(force) {
     this.acc.add(force);
-  }
-  
+  },
+
   this.update = function() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
-  }
-  
+  };
+
   this.draw = function() {
     point(this.pos.x, this.pos.y);
-  }
+  };
 }
