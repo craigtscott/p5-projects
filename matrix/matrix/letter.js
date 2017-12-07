dce9c09497195c48f99c74b92debd00e0d3f2fdc
Craig Scott <craigtscottjr@gmail.com>
2017-12-06 16:33:19 -0800

    matrix: letters now fall in a look

--- matrix/matrix/letter.js
+++ matrix/matrix/letter.js
@@ -1,9 +1,23 @@
-function Letter(){
-  this.letter = random(26) + 65;
+function Letter(x,y){
+  this.letter = floor(random(26) + 65);
+  this.pos = createVector(x,y);
   
   this.render = function(){
+    this.fall();
+    if (random(1) < .01) {
+      this.letter = floor(random(26) + 65)
+    }
+    
     stroke(0, 255, 85);
-    textSize(100);
-    text(char(floor(this.letter)), 300,300);
+    textSize(40);
+    text(char(this.letter), this.pos.x, this.pos.y);
+  }
+  
+  this.fall = function(){
+    this.pos.y += 10;
+    if (this.pos.y > height) {
+      this.pos.y -= height;
+    }
+    
   }
 }
