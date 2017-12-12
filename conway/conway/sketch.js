43107c50112afee452d3eacfaad48e2c95518dc9
Craig Scott <craigtscottjr@gmail.com>
2017-12-11 16:46:13 -0800

    conway: how has full screen

--- conway/conway/sketch.js
+++ conway/conway/sketch.js
@@ -8,10 +8,12 @@ function makeFeild(cols, rows) {
 let feild;
 let cols;
 let rows;
-let resolution = 40;
+let resolution = 10;
 
 function setup() {
-  createCanvas(400,400);
+  winWid = windowWidth - (windowWidth % 10);
+  winHe = windowHeight - (windowHeight % 10);
+  createCanvas(winWid,winHe);
   cols = width / resolution;
   rows = height / resolution;
   feild = makeFeild(cols, rows);
@@ -31,9 +33,42 @@ function draw() {
       let x = i * resolution;
       let y = j * resolution;
       if (feild[i][j] === 1){
+        stroke(0);
         fill(255);
         rect(x,y,resolution, resolution);
       }
     }
   }
+
+  let nextFeild = makeFeild(cols, rows);
+
+  for (let i = 0; i < feild.length; i++) {
+    for (var j = 0; j < feild[i].length; j++) {
+      let state = feild[i][j];
+      let sum = 0;
+      let adjCount = getAdj(feild, i, j);
+
+      if (state === 0 && adjCount === 3) {
+        nextFeild[i][j] = 1;
+      } else if (state === 1 && (adjCount < 2 || adjCount > 3)) {
+        nextFeild[i][j] = 0;
+      } else {
+        nextFeild[i][j] = state;
+      }
+    }
+  }
+  feild = nextFeild;
 }
+
+  function getAdj(feild, x, y){
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += feild[col][row];
+    }
+  }
+  sum -= feild[x][y];
+  return sum;
+  }
