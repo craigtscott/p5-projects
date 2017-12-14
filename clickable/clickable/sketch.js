f3b2a76de489f68582bd08cac67992836f318bc1
Craig Scott <craigtscottjr@gmail.com>
2017-12-14 15:20:09 -0800

    clickable: box changes color

--- clickable/clickable/sketch.js
+++ clickable/clickable/sketch.js
@@ -1,8 +1,45 @@
-
+var box = [];
 function setup() {
-
+  createCanvas(600,600);
+  box = [new Box()];
 }
 
 function draw() {
+  background(51);
+  for (var i = 0; i < box.length; i++) {
+    box[i].render();
+  }
+
+}
+
+function mouseClicked() {
+  this.pos = createVector(mouseX, mouseY);
+  console.log(this.pos);
+
+  for (var i = 0; i < box.length; i++) {
+    if( this.pos.x > box[i].pos.x &&
+        this.pos.x < box[i].pos.x + 50 &&
+        this.pos.y > box[i].pos.y &&
+        this.pos.y < box[i].pos.y + 50) {
+
+      if (box[i].color === 0) {
+        box[i].color = 255;
+      } else {
+        box[i].color = 0;
+      }
+    }
+  }
+}
+
+function Box() {
+  this.pos = createVector(height/2, width/2);
+  this.color = 0;
+
+  this.render = function() {
+    stroke(this.color);
+    fill(this.color);
+    rect(height/2, width/2, 50, 50);
+  };
+
 
 }
