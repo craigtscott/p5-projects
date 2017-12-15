7146e7d272176f430a7849072c0ecf4056fb04c9
Craig Scott <craigtscottjr@gmail.com>
2017-12-14 16:02:09 -0800

    clickey: finished

--- clickable/clickable/sketch.js
+++ clickable/clickable/sketch.js
@@ -1,44 +1,67 @@
-var box = [];
+var boxes = [];
+var locations = [ [225, 175], [275,175], [325, 175],
+                  [225, 225],
+                  [225, 275], [275,275], [325, 275],
+                  [225,325],
+                  [225, 375], [275,375], [325, 375]
+
+                ];
+var order = [];
+var pop = false;
 function setup() {
   createCanvas(600,600);
-  box = [new Box()];
+  for (var i = 0; i < locations.length; i++) {
+    boxes.push(new Box(locations[i]));
+  }
+
 }
 
 function draw() {
   background(51);
-  for (var i = 0; i < box.length; i++) {
-    box[i].render();
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].render();
   }
+  if (order.length === 11) {
+    pop = true;
+  }
+    frameRate(5);
+  if (order.length > 0 && pop === true) {
+    box = order.pop();
+    box.color = 0;
+  }
+
+  if(order.length === 0 && pop === true) {
+    pop = false;
+  }
+
 
 }
 
 function mouseClicked() {
   this.pos = createVector(mouseX, mouseY);
-  console.log(this.pos);
 
-  for (var i = 0; i < box.length; i++) {
-    if( this.pos.x > box[i].pos.x &&
-        this.pos.x < box[i].pos.x + 50 &&
-        this.pos.y > box[i].pos.y &&
-        this.pos.y < box[i].pos.y + 50) {
+  for (var i = 0; i < boxes.length; i++) {
+    if( this.pos.x > boxes[i].pos.x &&
+        this.pos.x < boxes[i].pos.x + 50 &&
+        this.pos.y > boxes[i].pos.y &&
+        this.pos.y < boxes[i].pos.y + 50 &&
+        boxes[i].color ===0 ) {
 
-      if (box[i].color === 0) {
-        box[i].color = 255;
-      } else {
-        box[i].color = 0;
-      }
+        boxes[i].color = 255;
+        order.push(boxes[i]);
+
     }
   }
 }
 
-function Box() {
-  this.pos = createVector(height/2, width/2);
+function Box(pos) {
+  this.pos =createVector(pos[0], pos[1]);
   this.color = 0;
 
   this.render = function() {
-    stroke(this.color);
+    stroke(51);
     fill(this.color);
-    rect(height/2, width/2, 50, 50);
+    rect(this.pos.x, this.pos.y, 50, 50);
   };
 
 
