935c86786345967812b06831860dd96b2817dfb7
Craig Scott <craigtscottjr@gmail.com>
2018-01-02 23:19:50 -0800

    createGraphics: spinny random dot thing

--- createGraphics/createGraphics/sketch.js
+++ createGraphics/createGraphics/sketch.js
@@ -1,23 +1,41 @@
 let graphics;
+let x = 50;
+let y = 50;
+let angle = 0;
 
 function setup() {
 createCanvas(400, 300);
 pixelDensity(1);
-graphics = createGraphics(400, 300);
-graphics.background(0);
+graphics = createGraphics(100, 100);
+graphics.background(50);
 
 }
 
 function draw() {
-
-  if (mouseIsPressed) {
-    graphics.fill(255);
-    graphics.stroke(255);
-    graphics.ellipse(mouseX, mouseY, 64);
-  }
+  background(0);
+  graphics.fill(255);
+  graphics.stroke(255);
+  graphics.ellipse(x, y, 15);
+  x += random(-5, 5);
+  y += random(-5, 5);
 
   image(graphics, 0, 0);
-  fill(255,255,0);
-  ellipse(mouseX, mouseY, 28);
+  push();
+  imageMode(CENTER);
+  translate(200,200);
+  rotate(angle);
+  image(graphics, 0, 0);
+  pop();
+
+  angle += 0.1;
+  // if (mouseIsPressed) {
+  //   graphics.fill(255);
+  //   graphics.stroke(255);
+  //   graphics.ellipse(mouseX, mouseY, 64);
+  // }
+  //
+  // image(graphics, 0, 0);
+  // fill(255,255,0);
+  // ellipse(mouseX, mouseY, 28);
 
 }
