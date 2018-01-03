94f7b7dbbb619309a7c8004dce83fdc6a115b0c3
Craig Scott <craigtscottjr@gmail.com>
2018-01-02 23:05:55 -0800

    createGraphics: test pen functional

--- createGraphics/createGraphics/sketch.js
+++ createGraphics/createGraphics/sketch.js
@@ -1,15 +1,23 @@
+let graphics;
+
 function setup() {
 createCanvas(400, 300);
-background(0);
+pixelDensity(1);
+graphics = createGraphics(400, 300);
+graphics.background(0);
 
 }
 
 function draw() {
 
   if (mouseIsPressed) {
-    fill(255);
-    stroke(255);
-    ellipse(mouseX, mouseY, 64);
+    graphics.fill(255);
+    graphics.stroke(255);
+    graphics.ellipse(mouseX, mouseY, 64);
   }
+
+  image(graphics, 0, 0);
+  fill(255,255,0);
+  ellipse(mouseX, mouseY, 28);
 
 }
