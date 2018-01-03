6aa29a0b8f75bd0bff55a41869d3fabbcc2b98cd
Craig Scott <craigtscottjr@gmail.com>
2018-01-02 22:57:17 -0800

    createGraphics: test functionality mouse press functional

--- createGraphics/createGraphics/sketch.js
+++ createGraphics/createGraphics/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
+createCanvas(400, 300);
+background(0);
 
 }
 
 function draw() {
-  
+
+  if (mouseIsPressed) {
+    fill(255);
+    stroke(255);
+    ellipse(mouseX, mouseY, 64);
+  }
+
 }
