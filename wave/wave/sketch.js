0cc92d1bafb3303a93bb23c7b1833cdf3892b8fd
Craig Scott <craigtscottjr@gmail.com>
2018-01-03 22:18:59 -0800

    wave: we have breathing line

--- wave/wave/sketch.js
+++ wave/wave/sketch.js
@@ -1,8 +1,17 @@
+let angle = 0;
 
 function setup() {
+createCanvas(600, 600);
 
 }
 
 function draw() {
+  background(0);
+  rectMode(CENTER);
+  let h = map(sin(angle), -1, 1, 0, 200);
+  fill(0,255, 0);
+  rect(300, 300, 20, h);
+
+  angle += 0.1;
 
 }
