7041ef9d446310f659daf27f30e36a0a4b90ad41
Craig Scott <craigtscottjr@gmail.com>
2018-01-03 22:38:50 -0800

    wave: line of berathing lines

--- wave/wave/sketch.js
+++ wave/wave/sketch.js
@@ -1,4 +1,6 @@
 let angle = 0;
+let w = 20;
+let width = 1200;
 
 function setup() {
 createCanvas(600, 600);
@@ -8,9 +10,14 @@ createCanvas(600, 600);
 function draw() {
   background(0);
   rectMode(CENTER);
-  let h = map(sin(angle), -1, 1, 0, 200);
-  fill(0,255, 0);
-  rect(300, 300, 20, h);
+  let offset = 0;
+  for (var x = 0; x < width; x += w) {
+    let ang = angle + offset;
+    let h = map(sin(ang), -1, 1, 0, 200);
+    fill(0,255, 0);
+    rect(x - width / 2 + w / 2, 300, w - 2, h);
+    offset += 0.1;
+  }
 
   angle += 0.1;
 
