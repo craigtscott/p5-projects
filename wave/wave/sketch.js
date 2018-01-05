8e70b7fee7785e52c61cda42ab657d3edc438ef8
Craig Scott <craigtscottjr@gmail.com>
2018-01-04 22:38:48 -0800

    wave: multi dimentional

--- wave/wave/sketch.js
+++ wave/wave/sketch.js
@@ -1,30 +1,34 @@
 let angle = 0;
-let w = 20;
-let width = 500;
+let w = 24;
+let ma;
+let maxD;
 
 function setup() {
-createCanvas(600, 600, WEBGL);
-
+  createCanvas(600, 600, WEBGL);
+  ma = atan(cos(QUARTER_PI));
+  maxD = dist(0, 0, 200, 200);
 }
 
 function draw() {
-  background(50);
-  directionalLight(255,255,255, 1, 0, 1);
-  translate(0, 50, -50);
-  rotateX(-PI / 8);
-  rectMode(CENTER);
+  background(100);
+  ortho(-400, 400, 400, -400, 0, 1000);
+  rotateX(-ma);
+  rotateY(-QUARTER_PI);
+
 
   let offset = 0;
-  for (var x = 0; x < width; x += w) {
-    push();
-    let ang = angle + offset;
-    let h = map(sin(ang), -1, 1, 0, 200);
-    ambientMaterial(0,255, 0);
-    //rect(x - width / 2 + w / 2, 300, 40, h);
-    translate(x - width/2 ,0 ,0);
-    box(w, h, w);
-    offset += 0.1;
-    pop();
+  for (var dep = 0; dep < height; dep += w) {
+    for (var x = 0; x < width; x += w) {
+      push();
+      let d = dist(x, dep, width / 2, height / 2);
+      let offset = map(d, 0, maxD, -PI, PI);
+      let ang = angle + offset;
+      let h = floor(map(sin(ang), -1, 1, 0, 200));
+      normalMaterial();
+      translate(x - width/2 , 0, dep - width/2);
+      box(w, h, w);
+      pop();
+    }
   }
 
   angle += 0.05;
