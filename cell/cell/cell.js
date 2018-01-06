34b7192029018841943255c71af8056c0c070a35
Craig Scott <craigtscottjr@gmail.com>
2018-01-05 23:24:36 -0800

    cell: cell has color

--- cell/cell/cell.js
+++ cell/cell/cell.js
@@ -1,6 +1,7 @@
 function Cell () {
   this.pos = createVector(random(width), random(height));
   this.r = 35;
+  this.c = color(0, random(100,255), random(100,255));
 
   this.move = function() {
     var vel = p5.Vector.random2D();
@@ -8,6 +9,8 @@ function Cell () {
   };
 
   this.show = function() {
+    fill(this.c);
+    noStroke();
     ellipse(this.pos.x, this.pos.y, this.r, this.r);
   };
 }
