bc6efa27484fd3e7727d417e30cf09130d5338bd
Craig Scott <craigtscottjr@gmail.com>
2018-01-22 22:17:23 -0800

    fractal: slider installed

--- fractal/fractal/sketch.js
+++ fractal/fractal/sketch.js
@@ -1,10 +1,13 @@
-
+var angle = .44879;
+var slider;
 function setup() {
   createCanvas(600,600);
+  slider = createSlider(0, PI, angle, 0.01);
 }
 
 function draw() {
   background(60);
+  angle = slider.value();
   stroke(255);
   translate(width/2, height);
   branch(100);
@@ -12,10 +15,14 @@ function draw() {
 }
 
 function branch(len){
+
   line(0, 0,0, - len);
   translate(0,-len);
-  rotate(PI/7);
-  branch(len* 0.75);
+  rotate(angle);
+  if (len > 5){
+    branch(len* 0.75);
+
+  }
 
 
 
