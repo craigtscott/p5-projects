fc03dfeca62ae215b4717dc665e73c7bd5261815
Craig Scott <craigtscottjr@gmail.com>
2018-01-25 21:33:38 -0800

    maze: grid up

---
+++ maze/maze/cell.js
@@ -0,0 +1,12 @@
+function Cell(i,j){
+  this.i = i;
+  this.j = j;
+
+  this.show = function() {
+    var x = this.i * w;
+    var y = this.j * w;
+    stroke(255);
+    noFill();
+    rect(x,y,w,w);
+  };
+}
--- maze/maze/sketch.js
+++ maze/maze/sketch.js
@@ -1,9 +1,23 @@
-
+var cols, rows;
+var w =40;
+var grid = [];
 function setup() {
   createCanvas(600,600);
+  cols = floor(width/w);
+  rows = floor(height/w);
+
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i,j);
+      grid.push(cell);
+    }
+  }
 
 }
 
 function draw() {
   background(60);
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].show();
   }
+}
