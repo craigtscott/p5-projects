87ca4f510a85f3c64b6f15bc0c4a0e114b5b106d
Craig Scott <craigtscottjr@gmail.com>
2018-02-02 21:50:42 -0800

    superShape: super Cricle

--- superShape/superShape/sketch.js
+++ superShape/superShape/sketch.js
@@ -6,5 +6,15 @@ function setup() {
 
 function draw() {
   background(70);
-
+  translate(width/2, height/2);
+  stroke(0,255,0);
+  noFill();
+  var rad = 200;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += 0.1) {
+    var x = rad * cos(a);
+    var y = rad * sin(a);
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
