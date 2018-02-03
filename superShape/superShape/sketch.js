ef59c6e483b48b9baa29a8c80c4c772d157e7360
Craig Scott <craigtscottjr@gmail.com>
2018-02-02 21:59:06 -0800

    superShape: super squair thing

--- superShape/superShape/sketch.js
+++ superShape/superShape/sketch.js
@@ -4,16 +4,32 @@ function setup() {
 
 }
 
+function sgn(num) {
+  if (num > 0) {
+    return 1;
+  } else if (num < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
 function draw() {
   background(70);
   translate(width/2, height/2);
   stroke(0,255,0);
   noFill();
   var rad = 200;
+  var a = 200;
+  var b = 200;
+  var n = 4;
   beginShape();
-  for (var a = 0; a < TWO_PI; a += 0.1) {
-    var x = rad * cos(a);
-    var y = rad * sin(a);
+  for (var ang = 0; ang < TWO_PI; ang += 0.1) {
+    var na = 2/n;
+    var x = pow(abs(cos(ang)), na) * a * sgn(cos(ang));
+    var y = pow(abs(sin(ang)), na) * b * sgn(sin(ang));
+    // var x = rad * cos(a);
+    // var y = rad * sin(a);
     vertex(x,y);
   }
   endShape(CLOSE);
