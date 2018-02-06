336164d6385b5864f3817582bd388aa7323369dc
Craig Scott <craigtscottjr@gmail.com>
2018-02-05 20:15:22 -0800

    superShape: increasing the complexity

--- superShape/superShape/sketch.js
+++ superShape/superShape/sketch.js
@@ -1,7 +1,21 @@
 var slider;
+
+var n1 = 1;
+var n2 = 1;
+var n3 = 1;
+var m = 1;
+var a = 1;
+var b = 1;
+
 function setup() {
   createCanvas(600,600);
   slider = createSlider(0,10,2,0.1);
+}
+
+function supershape(a) {
+  var r = 1;
+
+  return r;
 }
 
 function sgn(num) {
@@ -20,16 +34,15 @@ function draw() {
   stroke(0,255,0);
   noFill();
   var rad = 200;
-  var a = 200;
-  var b = 200;
+  // var a = 200;
+  // var b = 200;
   var n = slider.value();
   beginShape();
   for (var ang = 0; ang < TWO_PI; ang += 0.1) {
     var na = 2/n;
-    var x = pow(abs(cos(ang)), na) * a * sgn(cos(ang));
-    var y = pow(abs(sin(ang)), na) * b * sgn(sin(ang));
-    // var x = rad * cos(a);
-    // var y = rad * sin(a);
+    var rad = supershape(ang);
+    var x = 100 *rad * cos(ang);
+    var y = 100 * rad * sin(ang);
     vertex(x,y);
   }
   endShape(CLOSE);
