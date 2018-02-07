d1873c21617fd8f2db2c24e95afef2aba7abc0f7
Craig Scott <craigtscottjr@gmail.com>
2018-02-06 22:35:50 -0800

    mandelbrot: pixels set up

--- mandelbrot/mandelbrot/sketch.js
+++ mandelbrot/mandelbrot/sketch.js
@@ -1,7 +1,20 @@
 
 
 function setup() {
+  createCanvas(600, 600);
+  pixelDensity(1);
+  loadPixels();
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+      var pix = (x+y * width) *4;
+      pixels[pix + 0] = 70;
+      pixels[pix + 1] = 51;
+      pixels[pix + 2] = 51;
+      pixels[pix + 3] = 255;
 
+    }
+  }
+  updatePixels();
 }
 
 function draw() {
