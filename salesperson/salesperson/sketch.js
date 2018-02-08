93aa3c7c1fc3e5cc3b0980044547391403748773
Craig Scott <craigtscottjr@gmail.com>
2018-02-07 21:28:03 -0800

    salesperson: get random cities

--- salesperson/salesperson/sketch.js
+++ salesperson/salesperson/sketch.js
@@ -1,9 +1,18 @@
-
+var cities = [];
+var cityCount = 5;
 function setup() {
   createCanvas(600,600);
+
+  for (var i = 0; i < cityCount; i++) {
+    var v = createVector(random(width), random(height));
+    cities[i] = v;
+  }
 }
 
 function draw() {
   background(0);
+  for (var i = 0; i < cities.length; i++) {
+    ellipse(cities[i].x, cities[i].y, 5,5);
+  }
 
 }
