6ea77e436d1632c5d7c2ce0a5203a2a9f1ad731c
Craig Scott <craigtscottjr@gmail.com>
2018-02-12 13:59:02 -0800

    capture: adds button to take a snap and put it into the canvas

--- capture/capture/sketch.js
+++ capture/capture/sketch.js
@@ -1,13 +1,22 @@
 var video;
+var button;
+
 function setup() {
   createCanvas(640,480);
   background(50);
   video = createCapture(VIDEO);
-  video.hide();
+  // video.hide();
+  button = createButton('snap');
+  button.mousePressed(takesnap);
+
 
 }
 
 function draw() {
-  tint(0,100,50);
+  // tint(0,100,50);
+  // image(video, 0, 0);
+}
+
+function takesnap() {
   image(video, 0, 0);
 }
