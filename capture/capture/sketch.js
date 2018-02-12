0ff0ae3a814727fbede63151cd3ec5ff13e8765f
Craig Scott <craigtscottjr@gmail.com>
2018-02-12 14:06:26 -0800

    capture: show the first 5 images in canvas

--- capture/capture/sketch.js
+++ capture/capture/sketch.js
@@ -14,6 +14,9 @@ function setup() {
 }
 
 function draw() {
+  for (var i = 0; i < shots.length; i++) {
+    image(shots[i], i*(width/5), 0, width / 5, height / 5);
+  }
   // tint(0,100,50);
   // image(video, 0, 0);
 }
