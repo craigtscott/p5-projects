990e67cb321b42a2dcbfad0591e3b3e321b6c425
Craig Scott <craigtscottjr@gmail.com>
2018-02-13 22:43:49 -0800

    pix: grey scale

--- pix/pix/sketch.js
+++ pix/pix/sketch.js
@@ -1,18 +1,30 @@
+var video;
+
 function setup() {
   createCanvas(640,480);
   pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.size(640,480);
+
 }
 
 function draw() {
   background(50);
 
+  video.loadPixels();
   loadPixels();
   for (var x = 0; x < width; x++) {
     for (var y = 0; y < height; y++) {
         var idx = (x + y * width)*4;
-        pixels[idx] = x;
-        pixels[idx + 1] = 255;
-        pixels[idx + 2] = y;
+        var r = video.pixels[idx];
+        var b = video.pixels[idx+1];
+        var g = video.pixels[idx+2];
+
+        var bright = (r*b*g)/3;
+
+        pixels[idx] = bright;
+        pixels[idx + 1] = bright;
+        pixels[idx + 2] = bright;
         pixels[idx + 3] = 255;
     }
   }
