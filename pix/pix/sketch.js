a96efdd540c7f237c76ba231b5de32f1347931c0
Craig Scott <craigtscottjr@gmail.com>
2018-02-13 22:52:54 -0800

    pix: low resolution picture

--- pix/pix/sketch.js
+++ pix/pix/sketch.js
@@ -1,10 +1,11 @@
 var video;
+var vScale = 8;
 
 function setup() {
   createCanvas(640,480);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(640,480);
+  video.size(width/vScale, height/vScale);
 
 }
 
@@ -13,21 +14,18 @@ function draw() {
 
   video.loadPixels();
   loadPixels();
-  for (var x = 0; x < width; x++) {
-    for (var y = 0; y < height; y++) {
-        var idx = (x + y * width)*4;
+  for (var x = 0; x < video.width; x++) {
+    for (var y = 0; y < video.height; y++) {
+        var idx = (x + y * video.width)*4;
         var r = video.pixels[idx];
         var b = video.pixels[idx+1];
         var g = video.pixels[idx+2];
 
         var bright = (r*b*g)/3;
 
-        pixels[idx] = bright;
-        pixels[idx + 1] = bright;
-        pixels[idx + 2] = bright;
-        pixels[idx + 3] = 255;
+        fill(bright);
+        rect(x*vScale, y*vScale, vScale, vScale);
     }
   }
 
-  updatePixels();
 }
