0348e2d485b3430caafb69c9d6ab37ef1ae22b34
Craig Scott <craigtscottjr@gmail.com>
2018-02-13 22:36:15 -0800

    pix: adds gradents

--- pix/pix/sketch.js
+++ pix/pix/sketch.js
@@ -1,17 +1,21 @@
 function setup() {
   createCanvas(640,480);
-
+  pixelDensity(1);
 }
 
 function draw() {
   background(50);
 
   loadPixels();
-
-  pixels[0] = 255;
-  pixels[1] = 255;
-  pixels[2] = 255;
-  pixels[3] = 255;
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+        var idx = (x + y * width)*4;
+        pixels[idx] = x;
+        pixels[idx + 1] = 255;
+        pixels[idx + 2] = y;
+        pixels[idx + 3] = 255;
+    }
+  }
 
   updatePixels();
 }
