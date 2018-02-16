116d76e80d186455fdafb2be8894627e9ffc1b06
Craig Scott <craigtscottjr@gmail.com>
2018-02-15 21:33:01 -0800

    flocking: 100 particles painting the picture

--- flocking/flocking/sketch.js
+++ flocking/flocking/sketch.js
@@ -1,19 +1,25 @@
 var video;
 var vScale = 16;
-var paticle;
+var particles = [];
 
 function setup() {
   createCanvas(640,480);
   pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(width/vScale, height/vScale);
-  particle = new Particle(320,240);
+  for (var i = 0; i < 100; i++) {
+    particles[i] = new Particle(320,240);
+
+  }
   background(50);
 
 }
 
 function draw() {
   video.loadPixels();
-  particle.update();
-  particle.show();
-}
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show();
+  }
+
+  }
