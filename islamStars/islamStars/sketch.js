7ed17163f0839146c55680555bc68d199f0b07e8
Craig Scott <craigtscottjr@gmail.com>
2018-02-16 21:10:56 -0800

    islamStars: building up components polys and edges

--- islamStars/islamStars/sketch.js
+++ islamStars/islamStars/sketch.js
@@ -1,8 +1,36 @@
 
 function setup() {
+  createCanvas(600,600);
+  background(50);
 
 }
 
 function draw() {
 
+}
+
+function Polygon() {
+  this.edges = [];
+
+  this.addVertex = function(x,y) {
+    var a = createVertex(x,y);
+    this.vertices.push(a);
+  };
+
+  this.show = function() {
+    for (var i = 0; i < edges.length; i++) {
+      edges[i].show();
+    }
+  };
+
+}
+
+function Edge(a,b) {
+  this.a = a;
+  this.b = b;
+
+  this.show = function() {
+    stroke(255);
+    line(this.a.x, this.a.y, this.b.x, this.b.y);
+  };
 }
