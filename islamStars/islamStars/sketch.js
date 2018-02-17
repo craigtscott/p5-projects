7943ff02600bc6a281faf285598c2136bb8903a6
Craig Scott <craigtscottjr@gmail.com>
2018-02-16 21:14:23 -0800

    islamStars: new vertexies add new edges

--- islamStars/islamStars/sketch.js
+++ islamStars/islamStars/sketch.js
@@ -10,11 +10,18 @@ function draw() {
 }
 
 function Polygon() {
+  this.vertices = [];
   this.edges = [];
 
   this.addVertex = function(x,y) {
     var a = createVertex(x,y);
+    var total = this.vertices.length;
     this.vertices.push(a);
+    if (total > 0) {
+      var prev = this.vertices[total - 1];
+      var edge = new Edge(prev, a);
+      this.edges.push(edge);
+    }
   };
 
   this.show = function() {
