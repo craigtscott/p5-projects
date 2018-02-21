a70e6059f16fc694e9ce8bf0d3513c9f7beeb112
Craig Scott <craigtscottjr@gmail.com>
2018-02-20 21:57:23 -0800

    polarRose: starting to make shapes

--- polarRose/polarRose/sketch.js
+++ polarRose/polarRose/sketch.js
@@ -9,14 +9,16 @@ function draw() {
   background(0);
   translate(width/2, height/2);
 
+  beginShape();
+  stroke(255);
+  noFill();
   for (var a = 0; a < TWO_PI; a +=0.01){
-    var r = 100;
+    var r = 300 * cos(7*a);
     var x = r* cos(a);
     var y = r * sin(a);
-    stroke(255);
-    strokeWeight(5);
-    point(x,y);
+    vertex(x,y);
 
   }
+  endShape();
 
 }
