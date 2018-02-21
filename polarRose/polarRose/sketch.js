9c4301aa912918a489b8abb0facfc9e831d69c1a
Craig Scott <craigtscottjr@gmail.com>
2018-02-20 21:55:10 -0800

    polarRose: polar to cartisian circle

--- polarRose/polarRose/sketch.js
+++ polarRose/polarRose/sketch.js
@@ -7,5 +7,16 @@ function setup() {
 
 function draw() {
   background(0);
+  translate(width/2, height/2);
+
+  for (var a = 0; a < TWO_PI; a +=0.01){
+    var r = 100;
+    var x = r* cos(a);
+    var y = r * sin(a);
+    stroke(255);
+    strokeWeight(5);
+    point(x,y);
+
+  }
 
 }
