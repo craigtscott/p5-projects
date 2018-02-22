dd51af23da34dac3b15feabd4e5ed4530fb97057
Craig Scott <craigtscottjr@gmail.com>
2018-02-21 21:21:44 -0800

    polarRose: refactor slider

--- polarRose/polarRose/sketch.js
+++ polarRose/polarRose/sketch.js
@@ -3,7 +3,7 @@ var slider;
 
 function setup() {
   createCanvas(600,600);
-  slider = createSlider(1,50,4,1)
+  slider = createSlider(1,10,4,0.1)
 ;
 }
 
@@ -12,10 +12,11 @@ function draw() {
   translate(width/2, height/2);
   k = slider.value();
 
+
   beginShape();
   stroke(255);
   noFill();
-  for (var a = 0; a < TWO_PI; a +=0.01){
+  for (var a = 0; a < TWO_PI * 10; a +=0.01){
     var r = 300 * cos(k*a);
     var x = r* cos(a);
     var y = r * sin(a);
