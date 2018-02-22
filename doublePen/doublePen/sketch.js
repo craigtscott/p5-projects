44878dc56a41b820d0ab4ac91bb2440949be19f0
Craig Scott <craigtscottjr@gmail.com>
2018-02-21 21:43:34 -0800

    doublePen: tracer is added

--- doublePen/doublePen/sketch.js
+++ doublePen/doublePen/sketch.js
@@ -2,17 +2,24 @@ var r1 = 100;
 var r2 = 100;
 var m1  = 10;
 var m2 = 10;
-var ang1 = 3.14/2;
+var ang1 = 0;
 var ang2 = 0;
+let px2 = -1;
+let py2 = -1;
+let buffer;
 
 
 function setup() {
   createCanvas(600,600);
+  buffer = createGraphics(width, height);
+  buffer.background(0);
+  buffer.translate(width/2,height/2);
 }
 
 function draw() {
   background(0);
-
+  imageMode(CORNER);
+  image(buffer, 0, 0, width, height);
   translate(width/2,height/2);
 
   var x1 = r1 * sin(ang1);
@@ -24,8 +31,19 @@ function draw() {
 
   stroke(255);
   line(0,0,x1,y1);
-  ellipse(x1,y1,4,4);
+  ellipse(x1,y1,m1,m1);
 
   line(x1,y1,x2,y2);
-  ellipse(x2,y2,4,4);
+  ellipse(x2,y2,m2,m2);
+
+  ang1 += 0.1;
+  ang2 -= 0.2;
+
+  buffer.stroke(255);
+if (frameCount > 1) {
+  buffer.line(px2, py2, x2, y2);
+}
+
+px2 = x2;
+py2 = y2;
 }
