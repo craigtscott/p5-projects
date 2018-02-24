fb41bd4fe20cf53cd4036620a0967455eb07829f
Craig Scott <craigtscottjr@gmail.com>
2018-02-23 21:58:37 -0800

    phyllotaxis: colors

--- phyllotaxis/phyllotaxis/sketch.js
+++ phyllotaxis/phyllotaxis/sketch.js
@@ -2,6 +2,8 @@ var n = 0;
 var c = 3;
 function setup() {
   createCanvas(600,600);
+  angleMode(DEGREES);
+  colorMode(HSB);
   background(0);
 
 }
@@ -13,7 +15,7 @@ function draw() {
 
   var x = rad * cos(ang);
   var y = rad * sin(ang);
-
+  fill(ang % 256, 255, 255);
   ellipse(x,y,2);
 
   n++;
