b1e18f3b7db9b52ae46e5ba22c91594920f57146
Craig Scott <craigtscottjr@gmail.com>
2018-02-26 14:43:08 -0800

    2048: numbers combine

--- 2048/2048/sketch.js
+++ 2048/2048/sketch.js
@@ -27,6 +27,8 @@ function keyPressed() {
   if (key === ' ') {
     for (var i = 0; i < 4; i++) {
       grid[i] = slideRow(grid[i]);
+      grid[i] = combine(grid[i]);
+      // grid[i] = slideRow(grid[i]);
     }
   }
 
@@ -77,8 +79,19 @@ function addNumber() {
     let arr = row.filter(val => val);
     let mis = 4 - arr.length;
     let zeros = Array(mis).fill(0);
-    arr = arr.concat(zeros);
+    arr = zeros.concat(arr);
     return arr;
   }
 
-  function combine(){}
+  function combine(row){
+    for (var i = 3; i >= 1; i--) {
+      let a = row[i];
+      let b = row[i-1];
+      if (a === b && a !== 0) {
+        row[i] = a + b;
+        row[i+1] = 0;
+        break;
+      }
+    }
+    return row;
+  }
