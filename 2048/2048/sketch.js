dc0ea1a209bb205e0b1f798ab2ed8bdfc020420f
Craig Scott <craigtscottjr@gmail.com>
2018-02-26 13:51:37 -0800

    2048: grid populate in place

--- 2048/2048/sketch.js
+++ 2048/2048/sketch.js
@@ -17,16 +17,28 @@ function setup() {
 
 function draw() {
   background(0);
+  drawGrid();
+
+}
+
+function drawGrid() {
   for (var i = 0; i < 4; i++) {
     for (var j = 0; j < 4; j++) {
       noFill();
       strokeWeight(4);
       stroke(255);
       rect(i*wid, j*wid, wid,wid);
+      let val = grid[i][j];
+      if (grid[i][j] !== 0) {
+        textSize(24);
+        textAlign(CENTER);
+        strokeWeight(1);
+        text(val, (j * wid) + wid/2, (i * wid) + wid/2);
+      }
     }
   }
-}
 
+}
 function addNumber() {
   var opts = [];
   for (var i = 0; i < 4; i++) {
