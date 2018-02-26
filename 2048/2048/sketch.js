48325958981a621426612c87637931a2656762c1
Craig Scott <craigtscottjr@gmail.com>
2018-02-26 13:34:58 -0800

    2048: table and add number functional

--- 2048/2048/sketch.js
+++ 2048/2048/sketch.js
@@ -1,7 +1,38 @@
+var grid ;
 function setup() {
+  createCanvas(400,400);
+  grid = [  [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]
+          ];
 
+  console.table(grid);
+  addNumber();
+  addNumber();
+  console.table(grid);
 }
 
 function draw() {
+  background(0);
+}
+
+function addNumber() {
+  var opts = [];
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      if (grid[i][j] === 0) {
+        opts.push({
+          x: i,
+          y: j
+        });
+      }
+    }
+  }
+  if (opts.length > 0){
+    let spot = random(opts);
+    let ran = random(1);
+    grid[spot.x][spot.y] = ran > 0.5 ? 2 : 4;
+  }
 
 }
