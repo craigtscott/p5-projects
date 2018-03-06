0f59255f78f8c57692d2a4f06fc8614da4c64e1c
Craig Scott <craigtscottjr@gmail.com>
2018-03-05 21:41:56 -0800

    Diastic Machine: spitting out words

--- Diastic Machine/Diastic Machine/sketch.js
+++ Diastic Machine/Diastic Machine/sketch.js
@@ -7,9 +7,26 @@ function setup() {
   var submit = select("#submit");
   submit.mousePressed(function() {
     var phrase = diastic(seed.value(), words);
+    createP(phrase);
   });
+
 }
 
 function diastic(seed, arr) {
-  console.log(seed);
+
+  var phrase = "";
+  var currentWord = 0;
+
+  for (var i = 0; i < seed.length; i++) {
+    var c = seed.charAt(i);
+    for (var j = currentWord; j < arr.length; j++) {
+      if (arr[j].charAt(i) === c) {
+        phrase += words[j];
+        phrase += " ";
+        currentWord = j + 1;
+        break;
+      }
+    }
+  }
+  return phrase;
 }
