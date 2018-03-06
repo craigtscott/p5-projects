8b293e1457a1fe3785a5de582c243ba07579a6b2
Craig Scott <craigtscottjr@gmail.com>
2018-03-05 20:51:26 -0800

    Diastic Machine: setup IO

--- Diastic Machine/Diastic Machine/sketch.js
+++ Diastic Machine/Diastic Machine/sketch.js
@@ -2,6 +2,11 @@
 
 function setup() {
   noCanvas();
+  var seed = select("#seed");
+  var submit = select("#submit");
+  submit.mousePressed(function() {
+    console.log("summited");
+  });
 }
 
 function draw() {
