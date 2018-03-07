cbcddf5da76920ecd917c1f07af917bdd928a10f
Craig Scott <craigtscottjr@gmail.com>
2018-03-06 22:28:31 -0800

    wordcount: hashed all the words

--- wordcount/wordcount/sketch.js
+++ wordcount/wordcount/sketch.js
@@ -1,7 +1,14 @@
-
-
+var counts = {};
 function setup() {
-
+var tokens = srctxt.split(/\W+/);
+for (var i = 0; i < tokens.length; i++) {
+  var word = tokens[i];
+  if (counts[word] === undefined) {
+    counts[word] = 1;
+  } else {
+    counts[word]++;
+  }
+}
 }
 
 function draw() {
