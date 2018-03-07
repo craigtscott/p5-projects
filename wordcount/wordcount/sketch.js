288392d54ae762ed2f685c296e29f6e420a4bd55
Craig Scott <craigtscottjr@gmail.com>
2018-03-06 22:43:48 -0800

    wordcount: sort all the words in ferquency decending

--- wordcount/wordcount/sketch.js
+++ wordcount/wordcount/sketch.js
@@ -1,14 +1,27 @@
 var counts = {};
+var keys = [];
 function setup() {
 var tokens = srctxt.split(/\W+/);
-for (var i = 0; i < tokens.length; i++) {
-  var word = tokens[i];
-  if (counts[word] === undefined) {
-    counts[word] = 1;
-  } else {
-    counts[word]++;
+  for (var i = 0; i < tokens.length; i++) {
+    var word = tokens[i].toLowerCase();
+    if (counts[word] === undefined) {
+      counts[word] = 1;
+      keys.push(word);
+    } else {
+      counts[word]++;
+    }
   }
-}
+  keys.sort(compare);
+
+  function compare(a, b) {
+    var countA = counts[a];
+    var countB = counts[b];
+    return countB - countA;
+  }
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    createDiv(key + " " + counts[key]);
+  }
 }
 
 function draw() {
