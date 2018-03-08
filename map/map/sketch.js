2dfe2fb9e22fac93bd14c0942f531f460a47c815
Craig Scott <craigtscottjr@gmail.com>
2018-03-07 20:28:01 -0800

    map: dialed in the spot

--- map/map/sketch.js
+++ map/map/sketch.js
@@ -7,6 +7,7 @@ var hh = 512;
 // 27.9878° N, 86.9250° E
 var lat = 27.9878;
 var lon = 86.9250;
+
 var zoom = 0;
 
 function preload() {
@@ -34,15 +35,15 @@ function setup() {
 
 function mercX(lon) {
   lon = radians(lon);
-  var a = (256/PI) * pow(2,zoom);
-  var b = lon * PI;
+  var a = (512 / PI) * pow(2, zoom);
+  var b = lon + PI;
   return a * b;
 }
 
 function mercY(lat) {
   lat = radians(lat);
-  var a = (256/PI) * pow(2,zoom);
-  var b = tan(PI/4 + lat / 2);
+  var a = (512 / PI) * pow(2, zoom);
+  var b = tan(PI / 4 + lat / 2);
   var c = PI - log(b);
   return a * c;
 }
