a646ff0ba806d5a1299537ce32ee69a82734b4d4
Craig Scott <craigtscottjr@gmail.com>
2018-03-07 19:50:00 -0800

    map: display map on canvas throught mapbox

--- map/map/sketch.js
+++ map/map/sketch.js
@@ -1,7 +1,10 @@
-
+var mapImage;
+function preload() {
+  mapImage = loadImage("https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/-122.4241,37.78,14.25,0,60/600x600?access_token=pk.eyJ1IjoiY3JhaWd0c2NvdHRqciIsImEiOiJjamVod3pwbmUwZnZwMnhubDY1NTVjcDgyIn0.CZysnb_LuhsGj06WSMhkLA");
+}
 function setup() {
   createCanvas(600,600);
-
+  image(mapImage,0,0);
 }
 
 function draw() {
