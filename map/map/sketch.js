4087139e8c49b406f9c5a43061f2e028d57cd5db
Craig Scott <craigtscottjr@gmail.com>
2018-03-07 20:35:19 -0800

    map: display earth quakes

--- map/map/sketch.js
+++ map/map/sketch.js
@@ -9,13 +9,16 @@ var lat = 27.9878;
 var lon = 86.9250;
 
 var zoom = 0;
+var earthquakes;
 
 function preload() {
   mapImage = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
     clat + ',' + clon + ',' + zoom + '/' +
     ww + 'x' + hh +
     '?access_token=pk.eyJ1IjoiY3JhaWd0c2NvdHRqciIsImEiOiJjamVod3pwbmUwZnZwMnhubDY1NTVjcDgyIn0.CZysnb_LuhsGj06WSMhkLA');
+    earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
 }
+
 function setup() {
   createCanvas(ww,hh);
   translate(width/2, height/2);
@@ -25,12 +28,21 @@ function setup() {
   var cx = mercX(clon);
   var cy = mercY(clat);
 
-  var x = mercX(lon) - cx;
-  var y = mercY(lat) - cy;
-  console.log(x);
-  console.log(y);
-  fill(0, 110,16);
-  ellipse(x,y,10,10);
+  for (var i = 0; i < earthquakes.length; i++) {
+    var data = earthquakes[i].split(/,/);
+    var lat = data[1];
+    var lon = data[2];
+    var mag = data[4];
+    var x = mercX(lon) - cx;
+    var y = mercY(lat) - cy;
+    mag = pow(10, mag);
+    mag = sqrt(mag);
+    var magmax = sqrt(pow(10, 10));
+    var d = map(mag, 0, magmax, 0, 180);
+    fill(0, 110,16);
+    ellipse(x,y,10,10);
+  }
+
 }
 
 function mercX(lon) {
