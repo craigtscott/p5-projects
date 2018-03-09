e1b4a7ce87d8ad3a1d05cc5b4bc049dffe78cea7
Craig Scott <craigtscottjr@gmail.com>
2018-03-08 23:05:39 -0800

    steering: loads font

--- steering/steering/sketch.js
+++ steering/steering/sketch.js
@@ -1,4 +1,15 @@
+var font;
+
+function preload(){
+    font = loadFont('./TheBrownies.otf');
+}
 function setup() {
+  createCanvas(1024, 786);
+  background(27);
+  textFont(font, 128);
+
+  fill(255);
+  text('Wren', 10,200);
 
 }
 
