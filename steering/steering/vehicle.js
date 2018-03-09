55a596673a57888f457e2440cab693c4e82f61c9
Craig Scott <craigtscottjr@gmail.com>
2018-03-08 23:36:07 -0800

    steering: arived

--- steering/steering/vehicle.js
+++ steering/steering/vehicle.js
@@ -1,10 +1,11 @@
 function Vehicle(x,y){
-  this.pos = createVector(x,y);
+  this.pos = createVector(0,0);
   this.target = createVector(x,y);
   this.vel = p5.Vector.random2D();
   this.acc = createVector();
   this.rad = 8;
   this.max = 5;
+  this.force = 0.2;
 }
 
 
@@ -21,17 +22,32 @@ Vehicle.prototype.show = function() {
 };
 
 Vehicle.prototype.behaviors = function() {
-  var seek = this.seek(this.target);
-  this.applyForce(seek);
+  var arive = this.arived(this.target);
+  this.applyForce(arive);
 };
 
 Vehicle.prototype.applyForce = function(f) {
   this.acc.add(f);
 };
 
+Vehicle.prototype.arived = function(target) {
+  var desired = p5.Vector.sub(target, this.pos);
+  var dist = desired.mag();
+  var speed = this.max;
+  if (dist <100){
+    speed = map(dist,0,100,0,max);
+  }
+  desired.setMag(speed);
+  var steer = p5.Vector.sub(desired, this.vel);
+  steer.limit(this.force);
+  return steer;
+};
+
+
 Vehicle.prototype.seek = function(target) {
   var desired = p5.Vector.sub(target, this.pos);
   desired.setMag(this.max);
   var steer = p5.Vector.sub(desired, this.vel);
+  steer.limit(this.force);
   return steer;
 };
