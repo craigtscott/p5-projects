ebe2dcf01e0072f6f25cca3fd8a91ef16487ddf2
Craig Scott <craigtscottjr@gmail.com>
2018-03-09 13:18:23 -0800

    steering: text changes accoeding to text in input

--- steering/steering/sketch.js
+++ steering/steering/sketch.js
@@ -1,23 +1,29 @@
 var font;
 var vehicles = [];
-var input;
+var inp;
+var og;
 
 function preload(){
     font = loadFont('./TheBrownies.otf');
 }
 function setup() {
   createCanvas(1024, 500);
-  input = createInput("Wren");
+  inp = createInput("Wren");
+  updateText();
+  inp.input(updateText);
 
+}
 
-  var points = font.textToPoints(input.value(), 175,200,200);
-  console.log(points);
+function updateText(){
+  vehicles = [];
+  var points = font.textToPoints(inp.value(), 175,200,200);
 
   for (var i = 0; i < points.length; i++) {
     var poi = points[i];
     var vehicle = new Vehicle(poi.x, poi.y);
     vehicles.push(vehicle);
   }
+
 }
 
 function draw() {
