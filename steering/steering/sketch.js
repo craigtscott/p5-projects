dc9466be42a7c1b6a3d7f43a444ae7fd981dc255
Craig Scott <craigtscottjr@gmail.com>
2018-03-08 23:11:01 -0800

    steering: word into points

--- steering/steering/sketch.js
+++ steering/steering/sketch.js
@@ -6,11 +6,20 @@ function preload(){
 function setup() {
   createCanvas(1024, 786);
   background(27);
-  textFont(font, 128);
+  // textFont(font, 200);
+  //
+  // fill(255);
+  // text('Wren', 175,200);
 
-  fill(255);
-  text('Wren', 10,200);
+  var points = font.textToPoints('Wren', 175,200,200);
+  console.log(points);
 
+  for (var i = 0; i < points.length; i++) {
+    var poi = points[i];
+    stroke(0,255,0);
+    strokeWeight(4);
+    point(poi.x, poi.y);
+  }
 }
 
 function draw() {
