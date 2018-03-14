e76ae7a542bc3a521a72546cd27b454015b71f32
Craig Scott <craigtscottjr@gmail.com>
2018-03-13 18:07:40 -0700

    wordcount: text loadfile

--- wordcount/wordcount/sketch.js
+++ wordcount/wordcount/sketch.js
@@ -6,9 +6,9 @@ function preload(){
 }
 function setup() {
   console.log(words);
-  // var tokens = words.split(/\W+/);
-  for (var i = 0; i < words.length; i++) {
-    var word = words[i].toLowerCase();
+  var tokens = words.split(/\W+/);
+  for (var i = 0; i < tokens.length; i++) {
+    var word = tokens[i].toLowerCase();
     if (counts[word] === undefined) {
       counts[word] = 1;
       keys.push(word);
