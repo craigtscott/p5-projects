dc80b0f27048108545ce4db56a2b12a2a732828a
Craig Scott <craigtscottjr@gmail.com>
2018-03-14 22:27:04 -0700

    pi: setup files

--- pi/pi/sketch.js
+++ pi/pi/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
+  createCanvas(600,600);
+
 
 }
 
 function draw() {
+  background(51);
+  translate(height/2, width/2);
+
+  stroke(0,255,0);
+  noFill();
+  ellipse(0,0,300,300);
+  rect(-150,-150,300,300);
 
 }
