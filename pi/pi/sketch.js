9af99fdf23e8db323e38da1f65e127ec3e67161f
Craig Scott <craigtscottjr@gmail.com>
2018-03-14 22:31:00 -0700

    pi: adds points at random

--- pi/pi/sketch.js
+++ pi/pi/sketch.js
@@ -1,16 +1,27 @@
+var d = 300;
+var r = d/2;
+var x;
+var y;
 function setup() {
   createCanvas(600,600);
+  background(51);
 
 
 }
 
 function draw() {
-  background(51);
   translate(height/2, width/2);
 
   stroke(0,255,0);
   noFill();
-  ellipse(0,0,300,300);
-  rect(-150,-150,300,300);
+  ellipse(0,0,d,d);
+  rectMode(CENTER);
+  rect(0,0,d,d);
+
+  x = random(-r,r );
+  y = random(-r,r);
+
+  point(x,y);
+
 
 }
